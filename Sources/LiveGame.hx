361b34bd242dd26249f296b61dbb29844e29afeb
benwaah <benoit.wagler@gmail.com>
2016-03-04 15:00:49 +0100

    Replaced a still image with an animated wizard

--- Sources/LiveGame.hx
+++ Sources/LiveGame.hx
@@ -12,16 +12,27 @@ import kha.System;
 
 class LiveGame {
 
+	private static inline var FRAME_SIZE:Float = 64;
+	private static inline var FRAME_COUNT:Float = 3;
+	private static inline var FRAME_TIME:UInt = 50;
+
 	private var initialized:Bool = false;
+	private var xPos:Float = 0;
+	private var yPos:Float = 0;
+
 	private var font:Font;
-	private var grumpyCat:Image;
+	private var wizard:Image;
 	private var backbuffer:Image;
+
+	private var currentTime:Float = 0;
 
 	public function new() {
 		backbuffer = Image.createRenderTarget(800, 600);
 
+		setNewTime();
+
 		Assets.loadFont("AnonymousProB", onFontLoad);
-		Assets.loadImage("grumpy_cat_nope", onImageLoad);
+		Assets.loadImage("wizard", onWizardLoad);
 
 		System.notifyOnRender(render);
 		Scheduler.addTimeTask(update, 0, 1 / 60);
@@ -37,7 +48,7 @@ class LiveGame {
 
 		var g:Graphics = backbuffer.g2;
 		g.begin();
-		g.drawImage(grumpyCat, 0, 0);
+		g.drawSubImage(wizard, 100, 100, xPos, yPos, FRAME_SIZE, FRAME_SIZE);
 		g.end();
 
 		g = framebuffer.g2;
@@ -48,6 +59,21 @@ class LiveGame {
 		g.color = Color.Purple;
 		g.drawString("Hello world!", 0, 0);
 		g.end();
+
+		var now:Float = Scheduler.time() * 1000;
+		if (now < currentTime + FRAME_TIME)
+			return;
+
+		setNewTime();
+
+		xPos += FRAME_SIZE;
+		if (xPos > FRAME_SIZE * FRAME_COUNT)
+		{
+			xPos = 0;
+			yPos += FRAME_SIZE;
+		}
+		if (yPos > FRAME_SIZE * FRAME_COUNT)
+			yPos = 0;
 	}
 
 	private function onFontLoad(font:Font):Void {
@@ -55,12 +81,16 @@ class LiveGame {
 		checkInitialization();
 	}
 
-	private function onImageLoad(image:Image):Void {
-		grumpyCat = image;
+	private function onWizardLoad(image:Image):Void {
+		wizard = image;
 		checkInitialization();
 	}
 
 	private inline function checkInitialization():Void {
-		initialized = !((font == null) || (grumpyCat == null));
+		initialized = !((font == null) || (wizard == null));
+	}
+
+	private function setNewTime():Void {
+		currentTime = Scheduler.time() * 1000;
 	}
 }
